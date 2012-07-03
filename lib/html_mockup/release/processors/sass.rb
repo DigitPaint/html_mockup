9f369241bfeef034de8de7c844abd299bfc78ce7
Flurin Egger <flurin@digitpaint.nl>
2012-07-03 22:17:58 +0200

    Add stubs for release processors

---
+++ lib/html_mockup/release/processors/sass.rb
@@ -0,0 +1,5 @@
+# Use the sass gem
+module HtmlMockup::Release::Processors
+  class Sass < Base
+  end
+end
---
+++ lib/html_mockup/release/processors/yuicompressor.rb
@@ -0,0 +1,5 @@
+# Use the ruby-yui-compressor gem.
+module HtmlMockup::Release::Processors
+  class Yuicompressor < Base
+  end
+end
