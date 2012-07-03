84af99a40738eb8228ea5d27c43d825ba8e26a1c
Flurin Egger <flurin@digitpaint.nl>
2012-07-03 22:22:58 +0200

    Implement Dir finalizer

--- lib/html_mockup/release/finalizers/dir.rb
+++ lib/html_mockup/release/finalizers/dir.rb
@@ -7,6 +7,12 @@ require 'fileutils'
 module HtmlMockup::Release::Finalizers
   class Dir < Base
     
-
+    # @option options :prefix Prefix to put before the version (default = "html")
+    def call(release, options = {})
+      name = [(options[:prefix] || "html"), release.scm.version].join("-")
+      
+      release.log(self, "Finalizing release to #{release.target_path + name}")
+      FileUtils.cp_r release.build_path, release.target_path + name
+    end
   end
 end
