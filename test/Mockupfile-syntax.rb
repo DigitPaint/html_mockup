1874be473bd12e66ee2e6b2f5cb9d47b077fcc17
Flurin Egger <flurin@digitpaint.nl>
2012-10-31 11:20:08 +0100

    Update mockupfile-syntax and document examples

--- test/Mockupfile-syntax.rb
+++ test/Mockupfile-syntax.rb
@@ -4,63 +4,79 @@ Sass::Plugin.options[:style] = :expanded
 Sass::Plugin.options[:template_location] = "./html/stylesheets"
 Sass::Plugin.options[:css_location] = "./html/stylesheets"
 
-config = {}
-sass_config = {}
-sprockets_config = {}
-
-config[:root_path] = Pathname.new(File.dirname(__FILE__)) + "../html"
-config[:partial_path] = (config[:root_path] + "../partials/").realpath
-# config[:port] = 9000 # Should not really have to be configured
-config[:stylesheet_path] = "./html/stylesheets"
+# These are defaults, but can be set here
+# mockup.project.html_path = mockup.project.path + "html"
+# mockup.project.partial_path = mockup.project.path + "partials"
 
 mockup.serve(config) do |server|
-  server.use :sass, sass_config
-  server.use :sprockets, sprockets_config
+  server.use :sass
 end
 
 mockup.release(config) do |release|
   release.target_path # The target path where releases are put
   release.build_path # The path where the release gets built
-  release.release_path # The actual path where the release wil be put if done (can be a zip file too)
+  release.source_path # The source for this mockup
   
   # Extract mockup
   
   # Get git version
   release.scm.previous # Get the previous version SCM op (looks for tags)
-  
   release.scm.version # Get the git version
   release.scm.date # Get the git date
-  release.scm.sha1 # SHA1 commit
-  
-  # release.scm.tag! # is this possible?
-  
-  # Create banner (has a default)
+    
+  # Create custom banner
   release.banner do
     "bla bla bla"
   end
   
-  # Sassify CSS
-  release.use :sass, sass_config
+  # The default banner looks like this:
+  #
+  # =======================
+  # = Version : v1.0.0    =
+  # = Date : 2012-06-20   =
+  # =======================  
   
-  # Sprocketize JS
-  release.use :sprockets, sprockets_config
+  # Sassify CSS (this are the defaults too), all options except form :match and :skip are passed to Sass.compile_file
+  # release.use :sass, :match => ["stylesheets/**/*.scss"], :skip => [/_.*\.scss\Z/], :style => :expanded
+  # The previous statement is the same as:
+  release.use :sass
   
-  # Minify & add banners
-  release.use :yuicompressor, {}
+  # Run requirejs optimizer
+  # release.use :requirejs, {
+  #     :build_files => {"javascripts/site.build.js" => "javascripts"},
+  #     :rjs => release.source_path + "../vendor/requirejs/r.js",
+  #     :node => "node"
+  #   }
+  release.use :requirejs
+    
+  # Minify, will not minify anything above the :delimiter
+  # release.use :yuicompressor, {
+  #   :match => ["**/*.{css,js}"],
+  #   :skip =>  [/javascripts\/vendor\/.*\.js\Z/, /_doc\/.*/],
+  #   :delimiter => Regexp.escape("/* -------------------------------------------------------------------------------- */")
+  # }
+  # The previous statement is the same as:
+  release.use :yuicompressor
+  
   
   # Inject VERSION / DATE (i.e. in TOC)
-  release.inject({"VERSION" => release.version, "DATE" => release.date}, :into => %w{_doc/toc.html})
+  r.inject({"[VERSION]" => release.scm.version, "[DATE]" => release.scm.date.strftime("%Y-%m-%d")}, :into => %w{_doc/toc.html})
+  
+  # Inject Banners on everything matching the regexp in all .css files
+  # The banner will be commented as CSS.
+  release.inject({ /\/\*\s*\[BANNER\]\s*\*\// => r.banner(:comment => :css)}, :into => %w{**/*.css})
   
   # Inject CHANGELOG
-  release.inject({"CHANGELOG" => {:file => "", :processor => "md"}}, :into => %w{_doc/changelog.html})  
+  release.inject({"[CHANGELOG]" => {:file => "../CHANGELOG", :processor => 'md'}}, :into => %w{_doc/changelog.html})  
   
   # Inject NOTES
-  release.inject({"NOTES" => {:file => "", :processor => "md"}}, :into => %w{_doc/notes.html})  
+  release.inject({"[NOTES]" => {:file => "../NOTES.md", :processor => 'md'}}, :into => %w{_doc/notes.html})    
   
+  # Cleanup on the build
+  release.cleanup "**/.DS_Store"  
   
-  # Move to release / zip
-  release.finalize :zip
-  release.finalize :dir
-  release.finalize lambda{|build_path|  }
-  
+  # Finalize the release
+  # This is the default finalizer so not required
+  # release.finalize :dir
+    
 end
