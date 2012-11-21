c9d9f2a351e99635050a7277eac1967eef1610c9
Flurin Egger <flurin@digitpaint.nl>
2012-11-21 16:35:35 +0100

    Improve cleaner

--- lib/html_mockup/release/cleaner.rb
+++ lib/html_mockup/release/cleaner.rb
@@ -9,9 +9,11 @@ module HtmlMockup
       Dir.chdir(release.build_path.to_s) do
         Dir.glob(@pattern).each do |file|
           path = File.join(release.build_path.to_s, file)
-          release.log(self, "Cleaning up \"#{path}\" in build")
-          if inside_build_path release.build_path, path
-              rm_rf(path)
+          if is_inside_build_path(release.build_path, path)
+            release.log(self, "Cleaning up \"#{path}\" in build")
+            rm_rf(path)
+          else
+            release.log(self, "FAILED cleaning up \"#{path}\" in build")
           end
         end
       end
@@ -19,9 +21,21 @@ module HtmlMockup
 
     protected
 
-    def inside_build_path(build_path, pattern)
-      build_path = Pathname.new(build_path).realpath.to_s
-      path = Pathname.new(File.join(build_path.to_s, pattern)).realpath.to_s
+    def is_inside_build_path(build_path, path)     
+      
+      begin 
+        build_path = Pathname.new(build_path).realpath.to_s
+        path = Pathname.new(path)
+        if(path.absolute?)
+          path = path.realpath.to_s
+        else
+          path = Pathname.new(File.join(build_path.to_s, path)).realpath.to_s
+        end
+      rescue Errno::ENOENT
+        # Real path does not exist
+        return false
+      end
+      
       if path[build_path]
         return true
       else
--- test/unit/release/cleaner_test.rb
+++ test/unit/release/cleaner_test.rb
@@ -1,26 +1,42 @@
 require "./lib/html_mockup/release.rb"
 require "./lib/html_mockup/release/cleaner.rb"
-require "pry"
 require "test/unit"
 
 class CleanerTest < Test::Unit::TestCase
 
-  def test_only_clean_inside_build_path
-    pattern = "processors"
-    cleaner = HtmlMockup::Release::Cleaner.new(pattern)
-    inside_build_path = cleaner.send :inside_build_path, File.dirname(__FILE__), pattern
+  def test_only_clean_inside_build_path_relative
+    path = "processors"
+    cleaner = HtmlMockup::Release::Cleaner.new(path)
+    inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
 
     assert(inside_build_path, "Only delete content inside build_path")
   end  
+
+  def test_only_clean_inside_build_path_absolute
+    path = Pathname.new(File.dirname(__FILE__) + "/processors").realpath.to_s
+    cleaner = HtmlMockup::Release::Cleaner.new(path)
+    inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
+
+    assert(inside_build_path, "Only delete content inside build_path")
+  end  
+
   
   def test_dont_clean_outside_build_path
-    pattern = "../../../lib"
-    cleaner = HtmlMockup::Release::Cleaner.new(pattern)
+    path = "../../../lib"
+    cleaner = HtmlMockup::Release::Cleaner.new(path)
 
     assert_raise RuntimeError do
-      inside_build_path = cleaner.send :inside_build_path, File.dirname(__FILE__), pattern
+      inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
     end
 
   end
+  
+  def test_dont_fail_on_nonexistent_files
+    path = "bla"
+    cleaner = HtmlMockup::Release::Cleaner.new(path)
+
+    assert !cleaner.send(:is_inside_build_path, File.dirname(__FILE__), path), "Failed on nonexistent directories/files"
+
+  end  
 
 end
