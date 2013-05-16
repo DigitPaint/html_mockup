c4cbf894451d8caa7338b4f721a4fdb5e5ffdc3d
Flurin Egger <flurin@digitpaint.nl>
2013-05-16 18:07:52 +0200

    Add layouts path to project

--- lib/html_mockup/project.rb
+++ lib/html_mockup/project.rb
@@ -10,19 +10,21 @@ module HtmlMockup
     # @attr :html_path [Pathname] The path of the HTML mockup
     # @attr :partial_path [Pathname] The path for the partials for this mockup
     # @attr :mockupfile [Mockupfile] The Mockupfile for this project
-    attr_accessor :path, :html_path, :partial_path, :mockupfile
+    attr_accessor :path, :html_path, :partial_path, :layouts_path, :mockupfile
     
     def initialize(path, options={})
       @path = Pathname.new(path)
       
       @options = {
         :html_path => @path + "html",
-        :partial_path => @path + "partials"
+        :partial_path => @path + "partials",
+        :layouts_path => @path + "layouts"
       }.update(options)
       
       paths = mockup_paths(@options[:html_path], @options[:partials_path] || @options[:partial_path])
       self.html_path = paths[0]
       self.partial_path = paths[1]
+      self.layouts_path = @options[:layouts_path]
       
       @mockupfile = Mockupfile.new(self)
       @mockupfile.load
@@ -47,6 +49,11 @@ module HtmlMockup
     end
     alias :partials_path :partial_path
     alias :partials_path= :partial_path=
+    
+    def layouts_path=(p)
+      @layouts_path = Pathname.new(p).realpath
+    end
+    
         
     protected
     
