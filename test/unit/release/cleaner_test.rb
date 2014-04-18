19f6a2d3091435d60361c80ee31887cac85daa15
Flurin Egger <flurin@digitpaint.nl>
2014-04-18 19:05:32 +0200

    Fix cleaner test

--- test/unit/release/cleaner_test.rb
+++ test/unit/release/cleaner_test.rb
@@ -4,29 +4,34 @@ require "test/unit"
 
 class CleanerTest < Test::Unit::TestCase
 
+  def setup
+    @base = File.dirname(__FILE__) + "/../../project"
+  end
+
   def test_only_clean_inside_build_path_relative
-    path = "processors"
-    cleaner = HtmlMockup::Release::Cleaner.new(path)
-    inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
+  
+    cleaner = HtmlMockup::Release::Cleaner.new(@base)
+    inside_build_path = cleaner.send :is_inside_build_path, @base, @base + "/html/formats"
 
     assert(inside_build_path, "Only delete content inside build_path")
   end  
 
   def test_only_clean_inside_build_path_absolute
-    path = Pathname.new(File.dirname(__FILE__) + "/processors").realpath.to_s
+    path = Pathname.new(@base).realpath.to_s
     cleaner = HtmlMockup::Release::Cleaner.new(path)
-    inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
+
+    inside_build_path = cleaner.send :is_inside_build_path, path, @base + "/html/formats"
 
     assert(inside_build_path, "Only delete content inside build_path")
   end  
 
   
   def test_dont_clean_outside_build_path
-    path = "../../../lib"
+    path = File.dirname(__FILE__)
     cleaner = HtmlMockup::Release::Cleaner.new(path)
 
     assert_raise RuntimeError do
-      inside_build_path = cleaner.send :is_inside_build_path, File.dirname(__FILE__), path
+      inside_build_path = cleaner.send :is_inside_build_path, path, @base + "/html/formats"
     end
 
   end
@@ -35,7 +40,7 @@ class CleanerTest < Test::Unit::TestCase
     path = "bla"
     cleaner = HtmlMockup::Release::Cleaner.new(path)
 
-    assert !cleaner.send(:is_inside_build_path, File.dirname(__FILE__), path), "Failed on nonexistent directories/files"
+    assert !cleaner.send(:is_inside_build_path, @base + "/html/formats", path), "Failed on nonexistent directories/files"
 
   end  
 
